83897f0c33f4a7b3978dc86ef32551d075de8c91
Your Name <you@example.com>
2015-08-23 12:47:28 +0600

    init

---
+++ trunk/Zxsmysql.class.php
@@ -0,0 +1,11 @@
+<?php
+class Zxsmysql{
+	public $host;
+	public $user;
+	public $pwd;
+	public $dbname;
+	public function _construct($host,$user,$pwd,$dbname){
+		$this->host=
+	}
+}
+?>
---
+++ trunk/mysqlClass.php
@@ -0,0 +1,21 @@
+﻿<?php
+//这是一个封状了mysql数据库基本操作的类.
+class mysqlClass{
+
+//数据库连接函数mysqlConnect()
+	function mysqlConnect(){
+		$mysqlhost=array("host"=>"localhost","user"=>"acruxcn_zxs","pwd"=>"mmhhsx3685");
+		$conn=mysql_connect($mysqlhost['host'],$mysqlhost['user'],$mysqlhost['pwd']) or die("数据库连接失败...");
+		if($conn) echo "<1>.数据库连接成功!<br>";
+	}
+//数据库插入函数into()
+
+
+
+}
+
+//类测试程序
+	$newConn=new mysqlClass();
+	$newConn->mysqlConnect();
+	echo "<br>";
+?>
